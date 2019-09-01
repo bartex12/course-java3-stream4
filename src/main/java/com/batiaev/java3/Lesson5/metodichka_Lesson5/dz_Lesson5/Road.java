7acbf047dc6716a624b4e3b561ee5b20d5a477fb
Андрей <barcats@inbox.ru>
2019-09-01 12:43:40 +0300

    сделал дз по уроку 5 Java3 ещё до вебинара

---
+++ src/main/java/com/batiaev/java3/Lesson5/metodichka_Lesson5/dz_Lesson5/Road.java
@@ -0,0 +1,4 @@
+package com.batiaev.java3.Lesson5.metodichka_Lesson5.dz_Lesson5;
+
+public class Road {
+}
---
+++ src/main/java/com/batiaev/java3/lesson4/less4_metodichka/deadLock/NoRun.java
@@ -0,0 +1,4 @@
+package com.batiaev.java3.lesson4.less4_metodichka.deadLock;
+
+public class NoRun {
+}
