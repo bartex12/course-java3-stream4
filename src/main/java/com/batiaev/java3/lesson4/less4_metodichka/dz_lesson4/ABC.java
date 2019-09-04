61a35916b571fb450f07cc687553683b8c5da837
Андрей <barcats@inbox.ru>
2019-09-04 21:05:45 +0300

    ==

--- src/main/java/com/batiaev/java3/lesson4/less4_metodichka/dz_lesson4/ABC.java
+++ src/main/java/com/batiaev/java3/lesson4/less4_metodichka/dz_lesson4/ABC.java
@@ -1,4 +1,75 @@
 package com.batiaev.java3.lesson4.less4_metodichka.dz_lesson4;
 
+
+//    Создать три потока, каждый из которых выводит определенную букву (A, B и C) 5 раз (порядок
+//– ABСABСABС). Используйте wait/notify/notifyAll.
 public class ABC {
+
+    private final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+    final int COUNT = 5;
+
+    public static void main(String[] args) {
+        ABC w = new ABC();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        Thread t3 = new Thread(() -> {
+            w.printC();
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < COUNT; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) { e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < COUNT; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) { e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < COUNT; i++) {
+                    while (currentLetter != 'C') {
+                        mon.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    mon.notifyAll();
+                }
+            } catch (InterruptedException e) { e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/com/batiaev/java3/lesson4/lesson4_Batiaev/homeWorkLesso4_batiaev/TextPrinter.java
+++ src/main/java/com/batiaev/java3/lesson4/lesson4_Batiaev/homeWorkLesso4_batiaev/TextPrinter.java
@@ -1,4 +1,38 @@
 package com.batiaev.java3.lesson4.lesson4_Batiaev.homeWorkLesso4_batiaev;
 
-public class TextPrinter {
+import java.util.function.Consumer;
+
+public class TextPrinter implements Runnable{
+    private final Controller monitor;
+    private final int count;
+    private final String text;
+    private final Consumer<String> action;
+
+    TextPrinter(String text, int count, Controller monitor,
+                Consumer<String> action) {
+        this.text = text;
+        this.monitor = monitor;
+        this.count = count;
+        this.action = action;
+    }
+
+    @Override
+    public void run() {
+        synchronized (monitor) {
+            for (int i = 0; i < count; i++) {
+                try {
+                    while (!monitor.getCurrent().equals(text)) {
+                        monitor.wait();
+                    }
+                    action.accept(text);
+                    Thread.sleep(100);
+                    monitor.move();
+                    monitor.notifyAll();
+                } catch (Exception e) {
+                    System.out.println("Step "+ i
+                            + " failed with exception: " + e.getMessage());
+                }
+            }
+        }
+    }
 }
